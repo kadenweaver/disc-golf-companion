6eede0dc64b79e7df3d7e7c1b163a01f80af9db9
kadenweaver <kadenpweaver@gmail.com>
2023-07-27 14:11:52 -0700

    amplify configuration

---
+++ src/exports.js
@@ -0,0 +1,10 @@
+const awsmobile = {
+  aws_project_region: "us-west-2",
+  aws_appsync_graphqlEndpoint:
+    "https://hoxsx5gp7rhohfbnzmnjj4nieq.appsync-api.us-west-2.amazonaws.com/graphql",
+  aws_appsync_region: "us-west-2",
+  aws_appsync_authenticationType: "API_KEY",
+  aws_appsync_apiKey: "da2-o3fpwj5yafcu7f6betxbdsh5gq",
+};
+
+export default awsmobile;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Amplify } from "aws-amplify";
-import awsmobile from "./aws-exports";
+import awsmobile from "./exports";
 import App from "./App.jsx";
 import "./index.css";
 
