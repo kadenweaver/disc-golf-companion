d17f7e5247b4778e9156e555a33ffba6d0e45efd
kadenweaver <kadenpweaver@gmail.com>
2023-07-27 12:08:29 -0700

    attempting to add backend records with create functionality

--- src/ResultsPage.jsx
+++ src/ResultsPage.jsx
@@ -1,13 +1,35 @@
 import { ResultsTable } from "./ResultsTable";
+import { DataStore } from "@aws-amplify/datastore";
+import { UserGameScore } from "./models";
 
 export function ResultsPage(props) {
   const { playerList } = props;
+
+  const saveUserScores = async () => {
+    const userGameScores = playerList.map(x =>
+      DataStore.save(
+        new UserGameScore({
+          user: x.name,
+          frontNine: true,
+          userScores: x.scores,
+        })
+      )
+    );
+    const resultArray = await Promise.allSettled(userGameScores);
+
+    for (let result of resultArray) {
+      if (result.status === "rejected") {
+        console.log(result.reason);
+      }
+    }
+  };
 
   return (
     <div>
       <h1>Results</h1>
       <div style={{ display: "flex", flexDirection: "column" }}>
         <ResultsTable playerList={playerList} />
+        <button onClick={saveUserScores}>Save Results</button>
       </div>
     </div>
   );
