6db1e3f81af702d0b8e0a4cf40384134bdd9d76e
kadenweaver <kadenpweaver@gmail.com>
2023-07-27 14:03:19 -0700

    idk

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Amplify } from "aws-amplify";
-import config from "./aws-exports";
+import awsmobile from "./aws-exports";
 import App from "./App.jsx";
 import "./index.css";
 
-Amplify.configure(config);
+Amplify.configure(awsmobile);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
